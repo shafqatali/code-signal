42dcffe12ad1e21fcef878e4651ebd98b6a2f13d
Shafqat Ali <shafqat@kco.ie>
2020-09-14 18:20:23 +0500

    no message

--- isIPv4Address/is_ipv4_address.js
+++ isIPv4Address/is_ipv4_address.js
@@ -1,6 +1,15 @@
-function isIPv4Address(inputString) {
-let ip = inputString.split('.');
-
-return ip.filter((e)=>{return e.match(/\D/g) || e > 255 || parseInt(e) != e;}).length == 0 && ip.length === 4;
-
+function isIPv4Address(entry) {
+    var blocks = entry.split(".");
+    if(blocks.length === 4) {
+        return blocks.every(function(block) {
+            if(isNaN(block)){
+                return false;
+            }
+            if(block.startsWith("0") && block.length > 1){
+                return false;
+            }
+            return parseInt(block,10) >=0 && parseInt(block,10) <= 255;
+        });
+    }
+    return false;
 }
